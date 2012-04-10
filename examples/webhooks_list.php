74028a38ddcb41420726db6fc960892e2474ff7c
Wes Widner <wes@werxltd.com>
2012-04-10 18:25:24 -0400

    adding webhooks example

---
+++ examples/webhooks_list.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This examples shows how Mandrill library is used to obtain information about a user.
+ */
+
+require('../Mandrill.php');
+require('config.php');
+
+Mandrill::call(array('type'=>'webhooks', 'call'=>'list'));
